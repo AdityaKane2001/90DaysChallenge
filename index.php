8b81763aeea79a7903d878c62c33bf41e20b1582
AdityaKane2001 <adityakane@gmail.com>
2020-12-19 17:42:50 +0530

    Index start

--- index.php
+++ index.php
@@ -1,1 +1,92 @@
 <?php  ?>
+<!doctype html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>Trackr</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" style= "color: #919191; font-size: 30px;" href="#">Trackr</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <div style="position: absolute;right: 20px;">
+
+
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link"  style="color: #919191;"aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" style="color: #919191;" href="tracker.php">Tracker</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"  style="color: #919191;" href="login.php">Login</a>
+        </li>
+
+      </ul>
+        </div>
+    </div>
+  </div>
+</nav>
+
+
+<!-- Actual page goes here-->
+
+  <div class="container-fluid bg-light" style="box-shadow: 0 5px 7px dimgray;">
+  <br>
+
+    <div class="container my-5 py-5 z-depth-1">
+
+      <section class="px-md-5 mx-md-5 text-center dark-grey-text">
+
+
+        <div class="row d-flex justify-content-center">
+
+
+          <div class="col-xl-6 col-md-8">
+
+            <h3><img
+                src="images/sym.png" style="height: 60px;width: 50px;">Trackr</h3>
+
+            <p style="color:#919191;font-family:Verdana, Geneva, Tahoma, sans-serif ;font-size:15px ;">
+            The web app for tracking your progress.
+            </p>
+
+
+
+          </div>
+
+
+        </div>
+      </div>
+</div>
+
+
+
+  <footer class="footer mt-auto py-3 bg-dark"
+      style="color:#919191;font-family: Verdana, Geneva, Tahoma, sans-serif;">
+
+
+
+
+
+
+      <div class="footer-copyright text-center py-3"
+        style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
+        <p>Made with 	<span style="color:red;">&#x2764;</span> by Aditya Kane and Shantanu Deshpande</p>
+      </div>
+
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+  </body>
+</html>
